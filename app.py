989a6be9a13bb2402f878a0a9d881c2ca4464a0a
ledunc <lydiaduncan.ld@gmail.com>
2022-08-13 09:51:38 +0100

    recently played

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request, url_for, session, redirect
+from flask import Flask, request, url_for, session, redirect, render_template, jsonify
 import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import time
 from creds import *
+
 
 app = Flask(__name__)
 
@@ -47,7 +48,7 @@ def get_tracks():
         all_songs += items
         if len(items) < 50:
             break
-    return str(len(all_songs))
+    return f"Your total number of saved tracks is {str(len(all_songs))}!"
 
 
 @app.route('/getArtists')
@@ -61,6 +62,33 @@ def get_artist():
     results = sp.current_user_top_artists(limit=20, offset=0, time_range='medium_term')
     for items in results['items']:
         return str(items['name'])
+
+
+@app.route('/getTopTracks')
+def get_top_tracks():
+    try:
+        token_info = get_token()
+    except:
+        print("user not logged in")
+        return redirect("/")
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    results = sp.current_user_top_tracks(limit=20, offset=0, time_range='medium_term')
+    for items in results['items']:
+        return str(items['name'])
+
+
+@app.route('/getRecentlyPlayed')
+def get_recently_played():
+    try:
+        token_info = get_token()
+    except:
+        print("user not logged in")
+        return redirect("/")
+    sp = spotipy.Spotify(auth=token_info['access_token'])
+    results = sp.current_user_recently_played(limit=20)
+    for idx, item in enumerate(results['items']):
+        track = item['track']
+        return jsonify(idx, track['artists'][0]['name'], " – ", track['name'])
 
 
 def get_token():
@@ -83,6 +111,5 @@ def create_spotify_oauth():
             scope="user-library-read")
 
 
-app.run(debug=True)
 if __name__ == "__main__":
     app.run(debug=True)
