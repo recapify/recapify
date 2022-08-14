7ed359ce48f11218aa2bdb7da1bb632f4177bdaf
KatieC97 <101760184+KatieC97@users.noreply.github.com>
2022-08-14 23:00:44 +0100

    add testing token

--- creds.template.py
+++ creds.template.py
@@ -5,3 +5,4 @@ client_SECRET = ''
 redirect_URI = 'http://127.0.0.1:5000/redirect'
 SSK = os.urandom(12)
 scope = "user-library-read", "user-top-read", "playlist-read-private", "user-read-recently-played"
+testing_token = '' 
